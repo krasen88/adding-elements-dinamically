2c3203aa56ececc149e0023b67d25bebdb813a77
Krasen Mihaylov <36487835+krasen88@users.noreply.github.com>
2023-01-27 00:30:30 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import "../scss/app.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-    for (let i = 0; i < 5; i++) {
-        let article = document.createElement("article");
-        article.classList.add("message");
-        article.innerHTML = "This is message " + (i+1);
-        document.body.appendChild(article);
-    }
+  const button = document.querySelector(".button");
+  let articles = [];
+  for (let i = 0; i < 5; i++) {
+    let article = document.createElement("article");
+    article.classList.add("message");
+    article.innerHTML = "This is message " + (i+1);
+    articles.push(article);
+  }
+  button.addEventListener("click", () => {
+    articles.forEach(article => {
+      document.body.appendChild(article);
+    });
+  });
 });
