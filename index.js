d5c20ee1de37f6662f43c3a26898bc1126420fc4
Krasen Mihaylov <36487835+krasen88@users.noreply.github.com>
2023-01-26 21:01:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+document.body.addEventListener('click', function() {
+    for (let i = 1; i <= 5; i++) {
+        let message = document.createElement('article');
+        message.classList.add('message');
+        message.innerHTML = `Sample text content for message ${i}`;
+        document.body.appendChild(message);
+    }
+});
