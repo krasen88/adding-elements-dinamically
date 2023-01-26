3b94c8d5153bddf5b6e3311f3056ae7edcbdc171
Krasen Mihaylov <36487835+krasen88@users.noreply.github.com>
2023-01-26 21:35:49 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.body.addEventListener('click', function() {
-    for (let i = 1; i <= 5; i++) {
-        let message = document.createElement('article');
-        message.classList.add('message');
-        message.innerHTML = `Sample text content for message ${i}`;
-        document.body.appendChild(message);
-    }
-});
